0b5b3bc8a16de49dce5f0eb123337e567c759a3d
Ben Tate <bentate165@gmail.com>
2024-02-04 16:07:28 -0500

    Trying to figure out unsplash API

--- js/newtab.js
+++ js/newtab.js
@@ -1,1 +1,11 @@
 
+const clientID = 'aPjeYvJLySFlqz0GbtMnraNFnogsm1pc-gk-9jxkKNA';
+let endpoint= 'https://api.unsplash.com/photos/random/?client_id=${clientID}';
+
+fetch(endpoint)
+.then(function (response) {
+    return response.json()
+})
+.then(function (jsonData) {
+    console.log(jsonData)    
+})
