36d5911fb88ab12fb8ecf7e70711b67cfdde1f8c
Ben Tate <bentate165@gmail.com>
2024-02-05 14:43:04 -0500

    Got edit name working

--- js/newtab.js
+++ js/newtab.js
@@ -3,22 +3,87 @@ document.addEventListener("DOMContentLoaded", function() {
 //string representing the current time
 let time = "";
 
-//div with the timer
+//div with the clock
 let timer = document.getElementById("currentTime");
 
 //greeting for user
 let greeting = document.getElementById("myGreeting");
 
-//whether or not to display seconds in time
+//name within greeting div (editable part of phrase)
+let name = document.getElementById("name");
+
+//whether or not to display seconds on the clock
 let precision = false;
 
+//updates greeting to have most recent name of user
+function restore_name(){
+    chrome.storage.sync.get(['name'], function(result) {
+
+        //update name if possible
+        if(result != null){
+            var current_name = result.name; //get most recent name
+            name.innerText=current_name; //update the text inside of greeting to have the most recent name
+            console.log("name restored");
+        }
+    });
+}
+
+restore_name();
 timer.addEventListener("mouseover", toggle, false);
 timer.addEventListener("mouseout", toggle, false);
 
-//greeting.ondblclick = function() {
-    //var new_greeting = prompt();
-    //greeting.innerText = ("Hello, " + new_greeting);
-//}
+greeting.addEventListener("dblclick", edit_name, false); 
+
+
+//triggered when user tries to edit their name 
+function edit_name(){
+
+    //allow them to edit their name
+    name.contentEditable="true"; 
+
+    //create events that trigger when user is done editing
+    document.addEventListener("click", click_off, false); 
+    greeting.addEventListener("keydown", on_return, false);
+};
+
+//when user clicks off of name, save the new text in chrome storage
+function click_off(event){
+
+    //if user clicked outside of greeting box
+    if(!greeting.contains(event.target)) {
+        console.log("Clicked off of name");
+        save_name();
+    }
+}
+
+//save updated text if user hits return
+function on_return(event){
+
+    if(event.key == "Enter"){
+        console.log("Hit return after editing name");
+        save_name();
+    }
+}
+
+//save name text in chrome storage
+function save_name(textbox){
+
+    //don't let user edit their name anymore
+    name.contentEditable="false"; 
+
+    //get rid of event listeners
+    document.removeEventListener("click", click_off, false);
+    greeting.removeEventListener("keydown", on_return, false);
+
+    //update name
+    var current_name = name.innerText;
+
+    //save the updated name in chrome storage (part 1: id for element, part 2:)
+    chrome.storage.sync.set({'name' : current_name}, function() {
+        console.log("Name saved as " + current_name);
+    });
+}
+
 
 function getTimeMinutes() {
     var today = new Date();
@@ -70,6 +135,5 @@ function toggle() {
 
 // get time every 1000 milliseconds
 setInterval(decide, 1000);
-
 
 });
